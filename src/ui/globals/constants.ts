a033121a529521887c4622cdc78b1c9c519a838b
Salvatore Di Salvo <119612231+sdisalvo-crd@users.noreply.github.com>
2026-04-02 11:07:16 +0100

    chore: updating constants (#1649)

--- src/ui/globals/constants.ts
+++ src/ui/globals/constants.ts
@@ -1,37 +1,39 @@
+// General constants
 const DISPLAY_NAME_LENGTH = 32;
 const SEED_PHRASE_SUGGESTIONS = 3;
 const MAX_FAVOURITES = 5;
 const DOUBLE_TAP_DELAY = 300;
+
+// Version requirements
+const ANDROID_MIN_VERSION = 14;
+const WEBVIEW_MIN_VERSION = 79;
+const IOS_MIN_VERSION = 18;
 
 // Links
 const SUPPORT_EMAIL = "mailto:support@veridian.id";
 const WEBSITE_LINK = "https://www.veridian.id";
 const DOCUMENTATION_LINK = "https://docs.veridian.id/";
 const ONBOARDING_DOCUMENTATION_LINK =
-  "https://docs.veridian.id/onboarding#ssi-agent-details";
+  "https://docs.veridian.id/onboarding#connect-to-veridian";
 const RECOVERY_DOCUMENTATION_LINK =
-  "https://docs.veridian.id/recovery#ssi-agent-details";
+  "https://docs.veridian.id/recovery#connect-to-veridian";
 const FEDERAL_DATA_PROTECTION_LINK = "https://www.edoeb.admin.ch/de";
 const DATA_PROTECTION_AUTHORITIES_LINK =
   "https://ec.europa.eu/justice/article-29/structure/data-protection-authorities/index_en.htm";
 
-const ANDROID_MIN_VERSION = 14;
-const WEBVIEW_MIN_VERSION = 79;
-const IOS_MIN_VERSION = 18;
-
 export {
-  WEBSITE_LINK,
   DISPLAY_NAME_LENGTH,
   SEED_PHRASE_SUGGESTIONS,
   MAX_FAVOURITES,
   DOUBLE_TAP_DELAY,
-  SUPPORT_EMAIL,
-  DOCUMENTATION_LINK,
-  ONBOARDING_DOCUMENTATION_LINK,
-  RECOVERY_DOCUMENTATION_LINK,
   ANDROID_MIN_VERSION,
   WEBVIEW_MIN_VERSION,
   IOS_MIN_VERSION,
+  SUPPORT_EMAIL,
+  WEBSITE_LINK,
+  DOCUMENTATION_LINK,
+  ONBOARDING_DOCUMENTATION_LINK,
+  RECOVERY_DOCUMENTATION_LINK,
   FEDERAL_DATA_PROTECTION_LINK,
   DATA_PROTECTION_AUTHORITIES_LINK,
 };
